91e3f1a942a22100c852252574716f65931528f4
sagnikmaity <sagnikmaity.7@gmail.com>
2021-03-16 12:14:41 +0530

    String assignment

---
+++ StringAssignmentFolder/src/StringPackage/StringAssignment1.java
@@ -0,0 +1,21 @@
+package StringPackage;
+
+public class StringAssignment1 {
+
+	public static void main(String[] args) {
+		String str = "Hello My Dear Friends";
+		
+		/*Challenge :  Write your logic here so that the output should be in reverse order. It should be displayed as
+		 * Friends Dear My Hello.
+		 * You need to reverse the String word by word. 
+		 * Limitation :  Do not use the reverse method
+		 */
+		String afterReverse="";
+		String[] everyWordFromStartToEnd= str.split(" ");
+		for(int i=everyWordFromStartToEnd.length-1;i>=0;i--) {
+			afterReverse+= " "+everyWordFromStartToEnd[i];
+		}
+		System.out.println(afterReverse.trim());
+	}
+
+}
---
+++ StringAssignmentFolder/src/StringPackage/StringAssignment2.java
@@ -0,0 +1,26 @@
+package StringPackage;
+
+public class StringAssignment2 {
+    public static void main(String[] args) {
+    	String url1= "https://www.cognizant.com/admin/policy/index.html";
+    	String url2= "https://www.cognizant.com/service.jsp";
+    	String url3= "https://www.cognizant.com/customer/products/local/showProducts.asp";
+    	
+    	String command1=(getCommand(url1));
+    	String command2=(getCommand(url2));
+    	String command3=(getCommand(url3));
+    	
+    	System.out.println("Command 1 : "+command1);
+    	System.out.println("Command 2 : "+command2);
+    	System.out.println("Command 3 : "+command3);
+    }
+
+	private static String getCommand(String url1) {
+		
+		int commandStartIndex= url1.lastIndexOf("/")+1;
+		int commandEndIndex= url1.lastIndexOf(".");
+		String commandLine= url1.substring(commandStartIndex,commandEndIndex);
+		return commandLine;
+		
+	}
+}
